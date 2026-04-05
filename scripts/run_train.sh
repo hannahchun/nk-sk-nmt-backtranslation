51b54991e0f17d3095dbe9edb50d10d3b0610d75
Chun <hchun7@bluehive3.bluehive.circ.private>
2026-04-04 23:17:50 -0400

    add script for training model

---
+++ scripts/run_train.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --job-name=nk_sk_nmt_baseline
+#SBATCH --output=../logs/train_%j.out
+#SBATCH --error=../logs/train_%j.err
+#SBATCH --time=10:00:00
+#SBATCH --mem=16G
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+
+# Print job info
+echo "Job ID: $SLURM_JOB_ID"
+echo "Node: $(hostname)"
+echo "Start time: $(date)"
+
+# Activate conda
+eval "$(conda shell.bash hook)"
+echo "conda initialized"
+conda activate DSCC451Project
+echo "environment activated"
+
+# Verify environment
+echo "Python: $(which python)"
+echo "Conda env: $CONDA_DEFAULT_ENV"
+
+# Run code
+cd /scratch/hchun7/nk-sk-nmt-backtranslation/src
+
+python train.py \
+  --train_file ../data/bilingual/train.tsv \
+  --validation_file ../data/bilingual/val.tsv \
+  --gradient_clip_val 1.0 \
+  --max_epochs 5 \
+  --default_root_dir ../output \
+  --gpus 1 \
+  --batch_size 4 \
+  --num_workers 3
+
+echo "End time: $(date)"
